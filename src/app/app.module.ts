e6bd66fd23d1248f5096a2426e1ac0841681c5d9
robainggg <robincastel2003@gmail.com>
2025-03-06 10:19:43 +0100

    maj mauvais import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms'; // Import du module
 import { AppComponent } from './app.component';
-import { CreerSaeComponent } from './creer-sae/creer-sae.component'; // Import du composant
 import { RouterModule } from '@angular/router';
 import { ListComponent } from './pages/soutenance/list/list.component';
 import { DetailComponent } from './pages/soutenance/detail/detail.component';
 import { routes } from './app.routes';
+import {CreerSaeComponent} from "./pages/creer-sae/creer-sae.component";
 
 @NgModule({
   declarations: [
